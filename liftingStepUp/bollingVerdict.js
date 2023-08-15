ac3dc9b57fffdbb80a5d0bc6650e4550efdcdeb8
rijanThapa <142093222+rijanThapa@users.noreply.github.com>
2023-08-15 21:03:55 +0545

    Add files via upload

---
+++ liftingStepUp/bollingVerdict.js
@@ -0,0 +1,7 @@
+function BollingVerdict(props){
+    if (props.celsius >= 100) {
+        return <p>The water would boil</p>;
+    }
+    return <p>The water would not boil</p>
+}
+export default BollingVerdict;
