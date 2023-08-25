6c3468dc90a136f976192f406a09e07f49a1aba5
rijanThapa <142093222+rijanThapa@users.noreply.github.com>
2023-08-25 12:53:23 +0545

    Add files via upload

---
+++ map/main.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Map from './map.js';
+
+export default function SimpleMap() { // Updated function name to start with an uppercase letter
+  const [zoomLevel, setZoomLevel] = useState(0);
+  return (
+    <>
+      Zoom level: {zoomLevel}x
+      <button onClick={() => setZoomLevel(zoomLevel + 1)}>+</button>
+      <button onClick={() => setZoomLevel(zoomLevel - 1)}>-</button>
+      <hr />
+      <Map zoomLevel={zoomLevel} />
+    </>
+  );
+}
